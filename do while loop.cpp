52e94c03ca621c8ca3bf2a2f1f6a0e108ad0d5a2
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-07-20 14:55:30 +0500

    Add files via upload

---
+++ do while loop.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//do{
+//statements
+//}while(condition);
+//decralation of variables:
+/*int x=1;
+do{
+    cout<<"X is:\t"<<x<<endl;
+    x=x+1;
+
+}
+while(x<5);
+  */
+ /* int num,sum=0;
+  do{
+    cout<<"Enter a number:";
+cin>>num;
+sum+=num;
+  }
+  while(num!=0);
+
+  cout<<"Sum is :"<<sum;
+
+    return 0;*/
+   /* int a=1;
+    do{
+        int b=1;
+        do{
+            cout<<a<<"\n";
+            b++;
+        }while(b<=3);
+        a++;
+    }while(a<=3);
+    */
+
+}
